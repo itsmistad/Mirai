7582166da3ad1b6acf0664e8a33ec89e158027bc
Derek Williamson <d@mistad.net>
2020-01-31 05:25:45 -0600

    Added base scripts and fixed gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,35 +3,25 @@
 const gulp = require('gulp');  
 const sass = require('gulp-sass');  
 const cleanCss = require('gulp-clean-css');
-const browserSync = require('browser-sync');
+const browserSync = require('browser-sync').create();
 const webroot = 'src/webroot/';
 
 gulp.task('sass', function (done) {  
-    gulp.src(webroot + 'scss/theme.scss')
-        .pipe(sass({includePaths: [webroot + 'scss']}))
-        .pipe(gulp.dest(webroot + 'css'));
+    gulp.src(webroot + 'scss/*.scss')
+        .pipe(sass({includePaths: [webroot + 'scss'], outputStyle: 'compressed'}))
+        .pipe(gulp.dest(webroot + 'css'))
+        .pipe(browserSync.stream());
     done();
 });
- 
-gulp.task('minify-css', () => {
-  return gulp.src(webroot + 'css/*.css')
-    .pipe(cleanCss({compatibility: 'ie8', debug: true}), (details) => {
-        console.log(`${details.name}: ${details.stats.originalSize}`);
-        console.log(`${details.name}: ${details.stats.minifiedSize}`);
-    })
-    .pipe(gulp.dest(webroot + 'css'));
-});
 
-gulp.task('browser-sync', function(done) {  
-    browserSync.init([webroot + 'css/*.css', 'js/*.js'], {
-        server: {
-            baseDir: "./"
-        }
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+        server: "./"
     });
-    done();
+
+    gulp.watch(webroot + 'scss/*.scss', ['sass']);
+    gulp.watch(webroot + 'js/*.js').on('change', browserSync.reload);
+    gulp.watch(webroot + '**/*.html').on('change', browserSync.reload);
 });
 
-gulp.task('default', gulp.series(['sass', 'minify-css', 'browser-sync'], function (done) {  
-    gulp.watch(webroot + 'scss/*.scss', gulp.series(['sass']));
-    done();
-}));
+gulp.task('default', gulp.series(['serve']));
