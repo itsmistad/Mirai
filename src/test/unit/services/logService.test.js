a43285d52410f19fc0ec7ea4d9c00c786a71c709
Derek Williamson <d@mistad.net>
2020-01-30 20:48:48 -0600

    Fixed test

---
+++ src/test/unit/services/logService.test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var assert = require('assert');
+
+describe('logService', function() {
+  describe('#log()', function() {
+    it('should test nothing', function(done) {
+      done()
+    });
+  });
+});
