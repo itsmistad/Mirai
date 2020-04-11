dd733344b059b4b91f7fba5ca85ef59b6aa1593c
Derek Williamson <d@mistad.net>
2020-04-11 01:36:43 -0500

    Removed "Disconnected a client" message

--- src/services/handlers/socket/disconnectHandler.js
+++ src/services/handlers/socket/disconnectHandler.js
@@ -1,16 +1,13 @@
 'use strict';
 
-let log;
 
 class DisconnectHandler {
-    constructor(root) {
+    constructor() {
         this.event = 'disconnect';
-        log = root.log;
     }
 
     // eslint-disable-next-line no-unused-vars
     handle(io, client, user, data) {
-        log.debug('Disconnected a client: ' + JSON.stringify(user));
     }
 }
 
